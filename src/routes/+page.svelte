b6ea4f3bc39ce1f16c753f646d1cc253be6eeaf6
ObserverPoa <42487844+ObserverPoa@users.noreply.github.com>
2023-02-05 01:04:33 +0900

    initial setup

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<h1>WakNoti</h1>
+<h3>우왁굳, 이세돌 방송 알림 텔레그램 채널 모음</h3>
+<p>뱅온, 뱅종, 방제 변경, 카테고리 변경, 포인트 예측 시작/취소/결과</p>
+
+<a href="https://t.me/waknoti" target="_blank" rel="noopener noreferrer">메인 채널</a>
+
+<style>
+    h1 {
+        color: #46c0a7;
+    }
+</style>
