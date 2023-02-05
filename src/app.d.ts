59ba6c8940edc6e4e7adb066656203f7411e4ddc
ObserverPoa <42487844+ObserverPoa@users.noreply.github.com>
2023-02-05 15:34:05 +0900

    chore: migrate to cloudflare pages

--- src/app.d.ts
+++ src/app.d.ts
@@ -5,7 +5,16 @@ declare global {
 		// interface Error {}
 		// interface Locals {}
 		// interface PageData {}
-		// interface Platform {}
+		interface Platform {
+			env: {
+				COUNTER: DurableObjectNamespace;
+			};
+			context: {
+				// eslint-disable-next-line @typescript-eslint/no-explicit-any
+				waitUntil(promise: Promise<any>): void;
+			};
+			caches: CacheStorage & { default: Cache }
+		}
 	}
 }
 
