dec9b9034cb495c080747ed7bff3a36e70495ed0
ObserverPoa <42487844+ObserverPoa@users.noreply.github.com>
2023-02-15 19:19:13 +0900

    modify main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,21 @@
 <h1>WakNoti</h1>
-<h3>우왁굳, 이세계 아이돌 방송 알림 텔레그램 채널</h3>
+<h3>이세계 아이돌, 우왁굳 방송 알림 텔레그램 채널</h3>
 
 <ul>
     <li>방송: 뱅온, 뱅종, 방제 변경, 카테고리 변경</li>
     <li>채널 포인트 예측: 시작, 취소, 결과</li>
-    <li>채팅 (추후 예정)</li>
+    <li>채팅</li>
 </ul>
 
-<p>멤버 별로 다양한 알림 단계의 방송 알림 채널이 있습니다.</p>
+<p>
+    <a href="https://t.me/waknoti" target="_blank" rel="noopener noreferrer">방송 알림 채널</a>
+</p>
+<p>
+    <a href="https://t.me/waknoti_chat" target="_blank" rel="noopener noreferrer">채팅 알림 채널</a>
+</p>
 
-<b>
-    <a href="https://t.me/waknoti" target="_blank" rel="noopener noreferrer">대표 채널</a>
-</b>
+<br>
 
-<br><br>
 <h4>* 멤버별 알림 채널 초대링크는 준비중입니다!</h4>
 
 <style>
