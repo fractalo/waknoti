39ba96e63c2d24125e3f260cfea03bd9af47acd8
ObserverPoa <42487844+ObserverPoa@users.noreply.github.com>
2023-02-18 20:10:07 +0900

    add tailwind css, daisyui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,31 @@
-<h1>WAKNoti</h1>
+<h1 id="title" class="text-3xl font-bold underline">WAKNoti</h1>
+
+<br>
+
 <h3>이세계 아이돌, 우왁굳 방송 관련 알림 텔레그램 채널 모음</h3>
 
+<br>
+
 <ul>
-    <li>방송: 뱅온, 뱅종, 방제 변경, 카테고리 변경</li>
-    <li>채널 포인트 예측: 시작, 취소, 결과</li>
-    <li>채팅</li>
+    <li>- 방송: 뱅온, 뱅종, 방제 변경, 카테고리 변경</li>
+    <li>- 채널 포인트 예측: 시작, 취소, 결과</li>
+    <li>- 채팅</li>
 </ul>
 
+<br>
+
 <p>
-    <a href="https://t.me/waknoti" target="_blank" rel="noopener noreferrer">방송 알림 채널</a>
-</p>
-<p>
-    <a href="https://t.me/waknoti_chat" target="_blank" rel="noopener noreferrer">채팅 알림 채널</a>
+    <a class="btn" href="https://t.me/waknoti" target="_blank" rel="noopener noreferrer">방송 알림 채널</a>
+    <a class="btn" href="https://t.me/waknoti_chat" target="_blank" rel="noopener noreferrer">채팅 알림 채널</a>
 </p>
 
 <br>
 
 <h4>* 멤버별 알림 채널 초대링크는 준비중입니다!</h4>
 
+
 <style>
-    h1 {
+    #title {
         color: #46c089;
     }
 </style>
